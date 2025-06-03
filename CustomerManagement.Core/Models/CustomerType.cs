b1c9137e1c025ea9804139e5852f0ac881be3bc8
NkvXness <leonakavexmusic@gmail.com>
2025-06-03 16:54:01 +0300

    Add helper classes for business logic
       Created fundamental model classes:
       - CustomerType: Enum defining three customer types (Regular, Wholesale, VIP)
    
       Features implemented:
       - Type-safe customer type enumeration
    
       These classes will be used by Customer and its derived classes.

---
+++ CustomerManagement.Core/Models/CustomerType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagement.Core.Models
+{
+    /// <summary>
+    /// Перечисление типов покупателей
+    /// </summary>
+    public enum CustomerType
+    {
+        /// <summary>
+        /// Обычный покупатель (стандартная скидка 0-5%)
+        /// </summary>
+        Regular = 0,
+
+        /// <summary>
+        /// Оптовый покупатель (скидка 10-20%, минимальный заказ от 10000 BYN.)
+        /// </summary>
+        Wholesale = 1,
+
+        /// <summary>
+        /// VIP покупатель (скидка 25-30%, бонусная программа)
+        /// </summary>
+        VIP = 2
+    }
+}

4cc5f1c1dbf13d5b3cb87c0c868a198471aa7414
NkvXness <leonakavexmusic@gmail.com>
2025-06-05 21:36:19 +0300

    Implement discount strategies and customer inheritance hierarchy
       Created complete discount strategy system:
    
       1. StandardDiscountStrategy (Strategies/):
          - For regular customers: 0-5% discount
          - Progressive discount based on purchase amount
          - Logic: 0% (<1k), 2% (1k-5k), 3% (5k-10k), 4% (10k-20k), 5% (>20k)
    
       2. WholesaleDiscountStrategy (Strategies/):
          - For wholesale customers: 10-20% discount
          - Minimum order amount validation (default: 10,000 RUB)
          - Progressive discount: 10% (10k-30k), 12% (30k-50k), 15% (50k-100k),
            18% (100k-200k), 20% (>200k)
          - Additional method: ValidateMinim
    
    umOrder()

---
+++ CustomerManagement.Core/Strategies/StandardDiscountStrategy.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagement.Core.Strategies
+{
+    /// <summary>
+    /// Стандартная стратегия скидок для обычных покупателей
+    /// Скидка зависит от суммы покупки: 0-5%
+    /// </summary>
+    public class StandardDiscountStrategy : IDiscountStrategy
+    {
+        /// <summary>
+        /// Название стратегии
+        /// </summary>
+        public string StrategyName => "Стандартная скидка";
+
+        /// <summary>
+        /// Рассчитать скидку на основе суммы покупки
+        /// </summary>
+        /// <param name="totalAmount">Общая сумма покупок</param>
+        /// <returns>Сумма скидки в рублях</returns>
+        public decimal CalculateDiscount(decimal totalAmount)
+        {
+            decimal percentage = GetDiscountPercentage(totalAmount);
+            return totalAmount * (percentage / 100);
+        }
+
+        /// <summary>
+        /// Получить процент скидки для данной суммы
+        /// Логика скидки:
+        /// - До 1000 BYN: 0% (без скидки)
+        /// - 1000-5000 BYN: 2%
+        /// - 5000-10000 BYN: 3%
+        /// - 10000-20000 BYN: 4%
+        /// - Свыше 20000 BYN: 5%
+        /// </summary>
+        /// <param name="totalAmount">Общая сумма покупок</param>
+        /// <returns>Процент скидки</returns>
+        public decimal GetDiscountPercentage(decimal totalAmount)
+        {
+            if (totalAmount < 1000)
+                return 0m;
+            else if (totalAmount < 5000)
+                return 2m;
+            else if (totalAmount < 10000)
+                return 3m;
+            else if (totalAmount < 20000)
+                return 4m;
+            else
+                return 5m;
+        }
+    }
+}
---
+++ CustomerManagement.Core/Strategies/WholesaleDiscountStrategy.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagement.Core.Strategies
+{
+    /// <summary>
+    /// Стратегия оптовых скидок для оптовых покупателей
+    /// Скидка от 10% до 20% в зависимости от объема закупки
+    /// </summary>
+    public class WholesaleDiscountStrategy : IDiscountStrategy
+    {
+        /// <summary>
+        /// Название стратегии
+        /// </summary>
+        public string StrategyName => "Оптовая скидка";
+
+        /// <summary>
+        /// Минимальная сумма заказа для оптового покупателя
+        /// </summary>
+        public decimal MinimumOrderAmount { get; set; }
+
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// Минимальный заказ: 10000 руб.
+        /// </summary>
+        public WholesaleDiscountStrategy()
+        {
+            MinimumOrderAmount = 10000m;
+        }
+
+        /// <summary>
+        /// Конструктор с параметром
+        /// </summary>
+        /// <param name="minimumOrderAmount">Минимальная сумма заказа</param>
+        public WholesaleDiscountStrategy(decimal minimumOrderAmount)
+        {
+            MinimumOrderAmount = minimumOrderAmount;
+        }
+
+        /// <summary>
+        /// Рассчитать скидку на основе суммы покупки
+        /// </summary>
+        /// <param name="totalAmount">Общая сумма покупок</param>
+        /// <returns>Сумма скидки в BYN</returns>
+        public decimal CalculateDiscount(decimal totalAmount)
+        {
+            // Если сумма меньше минимальной, скидка не применяется
+            if (totalAmount < MinimumOrderAmount)
+                return 0m;
+
+            decimal percentage = GetDiscountPercentage(totalAmount);
+            return totalAmount * (percentage / 100);
+        }
+
+        /// <summary>
+        /// Получить процент скидки для данной суммы
+        /// Логика оптовой скидки:
+        /// - Меньше минимума: 0% (не применяется)
+        /// - 10000-30000 BYN: 10%
+        /// - 30000-50000 BYN: 12%
+        /// - 50000-100000 BYN: 15%
+        /// - 100000-200000 BYN: 18%
+        /// - Свыше 200000 BYN: 20%
+        /// </summary>
+        /// <param name="totalAmount">Общая сумма покупок</param>
+        /// <returns>Процент скидки</returns>
+        public decimal GetDiscountPercentage(decimal totalAmount)
+        {
+            if (totalAmount < MinimumOrderAmount)
+                return 0m;
+            else if (totalAmount < 30000)
+                return 10m;
+            else if (totalAmount < 50000)
+                return 12m;
+            else if (totalAmount < 100000)
+                return 15m;
+            else if (totalAmount < 200000)
+                return 18m;
+            else
+                return 20m;
+        }
+
+        /// <summary>
+        /// Проверить, соответствует ли сумма заказа минимальным требованиям
+        /// </summary>
+        /// <param name="orderAmount">Сумма заказа</param>
+        /// <returns>True если заказ соответствует минимуму</returns>
+        public bool ValidateMinimumOrder(decimal orderAmount)
+        {
+            return orderAmount >= MinimumOrderAmount;
+        }
+    }
+}
